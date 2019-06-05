b54d5faf51a0f588719db0a94c4a9ca6efe5d924
chimame <rito.tamata@gmail.com>
2019-06-05 23:24:28 +0900

    Modify split sample page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import Notification from './components/Notification'
 
-const App: React.FC = () => {
+const Top: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link className="App-link" to="/notification">Notification Sample</Link>
       </header>
-      <Notification />
     </div>
-  );
+  )
 }
 
-export default App;
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Top} />
+        <Route path="/notification" exact component={Notification} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
